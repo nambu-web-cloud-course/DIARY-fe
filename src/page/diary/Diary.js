68951d27caa14afd0ebec4223e5f31fefac78248
carpe-horas <carpehoras@hotmail.com>
2023-11-17 18:05:14 +0900

    오류 수정중

--- src/page/diary/Diary.js
+++ src/page/diary/Diary.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+//import React, { useState, useEffect } from "react";
+//import { Link } from "react-router-dom";
 
 
 export default function Diary(){
@@ -14,7 +14,7 @@ export default function Diary(){
 //       },
 //     };
 
-//     // 사용자가 저장한 일기 불러오기
+//     //사용자가 저장한 일기 불러오기
 //     axios.get(`http://localhost:8080/diaries/${userId}`, config)
 //       .then(response => {
 //         setDiaries(response.data); // 서버에서 받아온 일기 목록을 state에 저장
@@ -34,12 +34,11 @@ export default function Diary(){
 //         {/* diary */}
 
 
-//         {diaries.map(diary => (
-//             <div key={diary.id}>
-//             <h2>{diary.diary_title}</h2>
-//             <img src={diary.diary_content} alt={`이미지 - ${diary.diary_title}`} />
-//             </div>
-//         ))}
+//          {diaries.map(diary => (
+//              <div key={diary.id}>
+//              <img src={diary.diary_content} alt={`이미지 - ${diary.diary_title}`} />
+//              </div>
+//          ))}
         
 
 //         <div className="page-diary" >
@@ -47,47 +46,47 @@ export default function Diary(){
 //             <div class="page-title">
 //                 내가 쓴 일기
 //             </div>
-//             <div className="wrap-box">
-//                 {/* Box */}
-//                 <div className="box-diary">
-//                     <div className="box-title" key={diary.id}>
-//                         <span className="data-category">[카테고리명] </span>
+//              <div className="wrap-box">
+//                  {/* Box */}
+//                  <div className="box-diary">
+//                      <div className="box-title" key={diary.id}>
+//                          <span className="data-category">[카테고리명] </span>
                         
-//                         <span className="data-name">
-//                         {diary.diary_title}
-//                         일기 제목일기 제목일기 제목일기 제목일기 제목일기 제목일기 제목
-//                         </span>
-//                     </div>
-//                     <div className="data-image">
-//                         {diary.diary_content}
-//                         내 일기 1
-//                         커버 이미지
-//                     </div>
+//                          <span className="data-name">
+//                          {diary.diary_title}
+//                          일기 제목일기 제목일기 제목일기 제목일기 제목일기 제목일기 제목
+//                          </span>
+//                      </div>
+//                      <div className="data-image">
+//                          {diary.diary_content}
+//                          내 일기 1
+//                          커버 이미지
+//                      </div>
                     
-//                 </div>
-//                     {/* Box */}
-//                     <div className="box-diary">
-//                     <div className="box-title">
-//                         <span className="data-category">[카테고리명] </span>
-                        
-//                         <span className="data-name">
-//                             일기 제목일기 
-//                         </span>
-//                     </div>
-//                     <div className="data-image">
-//                         내 일기 1
-//                         커버 이미지
-//                     </div>
+//                  </div>
+//                      {/* Box */}
+//                      <div className="box-diary">
+//                      <div className="box-title">
+//                          <span className="data-category">[카테고리명] </span>
+                       
+//                          <span className="data-name">
+//                              일기 제목일기 
+//                          </span>
+//                      </div>
+//                      <div className="data-image">
+//                          내 일기 1
+//                          커버 이미지
+//                      </div>
                     
-//                 </div>
-//             </div>
+//                  </div>
+//              </div>
 
 
-//             <div className="fix-buttons">
-//                 <Link to="/mydiaries" className="form-button">새 일기 쓰기</Link>
-//                 {/* <a href="/mydiaries" className="form-button">새 일기 쓰기</a> */}
-//             </div>
-//         </div>
-//       </>
-    // )
+//              <div className="fix-buttons">
+//                  <Link to="/mydiaries" className="form-button">새 일기 쓰기</Link>
+//                  {/* <a href="/mydiaries" className="form-button">새 일기 쓰기</a> */}
+//              </div>
+//          </div>
+//        </>
+//      )
 }
---
+++ src/page/todolist/Todolist copy2.js
@@ -0,0 +1,243 @@
+import React, { useRef, useState, useEffect } from "react";
+import axios from "axios";
+
+const Todolist = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [searchTodo, setSearchTodo] = useState("");
+  const [error, setError] = useState("");
+  const contentRef = useRef();
+  const [checkedTodos, setCheckedTodos] = useState([]);
+  const [currentDate, setCurrentDate] = useState('');
+  const [selectedDate, setSelectedDate] = useState('');
+  
+  useEffect(() => {
+    // 저장된 JWT 토큰 가져오기
+    //const token = localStorage.getItem("token");
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtaWQiOjEsImlhdCI6MTcwMDE4ODQzMH0.0oFDWqt-H2M5qHVh9-jvA_J9_pliQ-feoDjtuQ8M-ok';
+
+    // 현재 날짜 가져오기 (YYYY-MM-DD 형식)
+    const today = new Date().toISOString().split('T')[0];
+    setCurrentDate(today);
+    
+    // 초기 선택된 날짜 설정
+    setSelectedDate(today);
+
+    if (token) {
+      axios
+        .get(
+          "/todos",
+          { validateStatus: false },
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+        .then((res) => {setTodos(res.data.data); 
+                      console.log(res.data.data)})
+        .catch((error) => setError(error.message));
+    }
+  },[]);
+
+
+  const addTodo = async (e) => {
+    e.preventDefault();
+
+    try {
+      //const token = localStorage.getItem("token");
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtaWQiOjEsImlhdCI6MTcwMDE4ODQzMH0.0oFDWqt-H2M5qHVh9-jvA_J9_pliQ-feoDjtuQ8M-ok';
+      setNewTodo(contentRef.current.value);
+      console.log(newTodo);
+
+      const response = await axios.post(
+        "https://diary-be.azurewebsites.net/todos",
+        { todo_content: newTodo, members_no:1},
+        //{ todo_content: newTodo, completed: false},
+        { headers: { Authorization: `Bearer ${token}` } 
+        }
+      );
+
+      console.log("서버 응답 야호!:", response.data);
+
+      if (response.data && response.data.success) {
+        setTodos([...todos, response.data.data]);
+      } else {
+        console.error("응답이 왜....?: ", response.data.data);
+      }
+      setNewTodo("");
+    } catch (error) {
+      console.error("에러ㅠㅠ:", error);
+    }
+  };
+
+
+
+  
+
+  //체크박스 상태 변경 핸들러
+  const handleCheckboxChange = (members_no) => {
+    setCheckedTodos((prevChecked) => {
+      if (prevChecked.includes(members_no)) {
+        return prevChecked.filter((id) => id !== members_no);
+      } else {
+        return [...prevChecked, members_no];
+      }
+    });
+  };
+
+
+    // 선택된 할일 삭제
+    const deleteTodo = () => {
+      //const token = localStorage.getItem("token");
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtaWQiOjEsImlhdCI6MTcwMDE4ODQzMH0.0oFDWqt-H2M5qHVh9-jvA_J9_pliQ-feoDjtuQ8M-ok';
+      if (checkedTodos.length === 0) {
+        alert("선택된 항목이 없습니다.");
+        return;
+      }
+      checkedTodos.forEach(async (checkedMemberNo) => {
+        try {
+          await axios.delete(`https://diary-be.azurewebsites.net/todos/${checkedMemberNo}`, {
+            headers: { Authorization: token },
+          });
+          setTodos((prevTodos) =>
+            prevTodos.filter((todos) => todos.members_no !== checkedMemberNo)
+          );
+        } catch (error) {
+          console.error("투두 삭제 오류:", error);
+        }
+      });
+
+      setCheckedTodos([]);
+  };
+
+  // 검색어 변경시 실행되는 함수
+  const handleSearch = (e) => {
+    setSearchTodo(e.target.value);
+  };
+
+  // 검색된 투두 리스트 필터링
+  const filteredTodos = searchTodo
+    ? todos.filter((todos) =>
+        todos.todo_content.toLowerCase().includes(searchTodo.toLowerCase())
+      )
+    : todos;
+
+
+    // 날짜 변경 핸들러
+    const handleDateChange = (action) => {
+    const currentDateObject = new Date(selectedDate);
+    let newDate;
+
+    if (action === 'prev') {
+      newDate = new Date(currentDateObject);
+      newDate.setDate(currentDateObject.getDate() - 1);
+    } else if (action === 'next') {
+      newDate = new Date(currentDateObject);
+      newDate.setDate(currentDateObject.getDate() + 1);
+    } else {
+      newDate = action;
+    }
+
+  //   const formattedDate = newDate.toISOString().split('T')[0];
+  //   setSelectedDate(formattedDate);
+  //   ChangeDateTodolist(formattedDate, localStorage.getItem('token'));
+  
+};
+
+
+
+
+
+  return (
+    <div>
+      {/* TodoList */}
+      <div className="page-todolist">
+        <div className="page-title">
+          Todo 리스트
+          <a href="/calendar" className="btn-calen">달력아이콘</a>
+          <img
+            src="calendar-icon.png"
+            alt="Calendar Icon"
+            className="btn-calen"
+          />
+        </div>
+        <div className="page-contents">
+          <div className="data-day">
+            <button onClick={()=>handleDateChange('prev')}>이전 </button>
+            {currentDate}
+            <button onClick={()=>handleDateChange('next')}> 다음</button>
+        </div>
+
+          {/* Form */}
+          <form className="form-type" onSubmit={addTodo}>
+            <input
+              ref={contentRef}
+              type="text"
+              id="newTodo"
+              name="todo_content"
+              placeholder="Todo 리스트를 추가해주세요 "
+              className="form-input"
+              value={newTodo}
+              onChange={(e) => setNewTodo(e.target.value)}
+            />
+            <button className="form-button">추가</button>
+          </form>
+
+          {/* Form */}
+          <div className="form-type">
+            <input
+              type="text"
+              id="searchTodo"
+              name="todo_content"
+              placeholder="Todo 리스트를 검색해주세요. "
+              className="form-input"
+              value={searchTodo}
+              onChange={handleSearch}
+              //onChange={(e) => setSearchTodo(e.target.value)}
+            />
+            <button
+              className="form-button"
+              onClick={() => setSearchTodo(searchTodo)}
+            >
+              검색
+            </button>
+          </div>
+
+          {/* List */}
+          <div className="wrap-list">
+            {/* Data 가 없을 경우 */}
+            {filteredTodos && filteredTodos.length === 0 && newTodo.trim() === "" ? (
+               <div className="data-nodata">
+                Todo 리스트가 없습니다.
+                <br />
+                Todo 리스트를 추가해주세요.
+              </div>
+            ) : (
+              <ul className="form-list">
+              {/* {filteredTodos.map((todo) => (*/}
+                {filteredTodos && filteredTodos.map((todo) => (
+                  <li key={todo.id}>
+                    <input type="checkbox" 
+                      checked={checkedTodos.includes(todo.todo_content)} 
+                      onChange={() => handleCheckboxChange(todo.todo_content)}
+                    />
+                  {todo.todo_content}
+                    <div className="data-list">{todo.members_no}</div>
+                    <button
+                      className="form-button type-s-dark"
+                      onClick={() => deleteTodo()}
+                    >
+                      삭제
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Todolist;
+
