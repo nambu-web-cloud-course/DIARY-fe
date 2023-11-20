61ea45b1bcef6986471ea454d744d67a768ec3c0
kyung <heppy-family@seong-wongyeonghwaui-MacBookPro.local>
2023-11-20 16:06:56 +0900

    calender, diaryedit 작업중

--- src/page/diary/DiaryEdit.js
+++ src/page/diary/DiaryEdit.js
@@ -5,25 +5,7 @@ import EditorForm from "../editor/EditorForm";
 function DiaryEdit() {
     return (
         <div className="wrap-page">
-            {/* Header */}
-          <div className="layout-header">
-            {/* Header Top */}
-            <div className="header-top">
-                <a href="javascript:" className="btn-back">뒤로가기 버튼</a>
-              <a href="javascript:" className="txt-logo">D.I.A.R.Y</a>
-              <div className="ui-right">
-                <div className="data-id">User id</div>
-                <a href="javascript:" className="link-login">로그인</a>
-                
-              </div>
-            </div>
-            <div className="header-menu">
-              <a href="javascript:">Todo 리스트</a>
-              <a href="javascript:">다이어리</a>
-              <a href="javascript:">캘린더</a>
-              <a href="javascript:">갤러리</a>
-            </div>
-          </div>
+         
 
             {/* TodoList */}
             <div className="page-diary">
---
+++ src/page/diary/DiaryEdit2.js
@@ -0,0 +1,78 @@
+import {useState} from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ReactHtmlParser from 'react-html-parser';
+import axios from 'axios';
+
+
+function DiaryEdit2() {
+    // const submitReview = ()=>{
+    //     axios.get('https://diary-be.azurewebsites.net/mydiaries/4')
+    //     .then(res => {
+    //         movieContent(res.data)
+    //     }).catch(error => {
+    //         console.log(error)
+    //     })
+
+    //   };
+
+  const [movieContent, setMovieContent] = useState({
+    title: '',
+    content: ''
+  });
+  const [viewContent, setViewContent] = useState([]);
+  const getValue = e => {
+    const { name, value } = e.target;
+      console.log(name, value);
+  };
+  return (
+    <div className="App">
+        <div>
+            {viewContent.map(element => 
+                <div>
+                    <h2>{element.title}</h2>
+                    <div>
+                    {ReactHtmlParser(element.content)}
+                    </div>
+                </div>
+            )}
+        </div>
+      <input className="title-input"
+        type='text'
+          placeholder='제목'
+          onChange={getValue}
+          name='title'
+      />
+      <CKEditor
+        editor={ ClassicEditor }
+        data="<p>Hello from CKEditor&nbsp;5!</p>"
+        onReady={ editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log( 'Editor is ready to use!', editor );
+        } }
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          console.log({ event, editor, data });
+          setMovieContent({
+            ...movieContent,
+            content: data
+          })
+          console.log(movieContent);
+        }}
+        onBlur={ ( event, editor ) => {
+            console.log( 'Blur.', editor );
+        } }
+        onFocus={ ( event, editor ) => {
+            console.log( 'Focus.', editor );
+        } }
+    />
+      {/* <button onClick={submitReview}>입력</button> */}
+      <button type="button" onClick={()=> {
+        setViewContent(viewContent.concat({...movieContent}))
+      }}>입력</button>
+    </div>
+  );
+}
+
+export default DiaryEdit2;
+
